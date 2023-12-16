830beec646541b182735cf0600ba0f97d358056c
Nikola Bursac <nikola.bursac@proton.me>
2023-12-16 10:23:47 +0100

    Add value object representing OAuth2 client asking for authorization

--- app/Domain/OAuth2/ValueObject/OAuth2Client.php
+++ app/Domain/OAuth2/ValueObject/OAuth2Client.php
@@ -2,7 +2,22 @@
 
 namespace App\Domain\OAuth2\ValueObject;
 
-class OAuth2Client
-{
+use Lcobucci\JWT\Signer;
 
+readonly class OAuth2Client
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $client_id,
+        public string $client_secret,
+        public array $redirect_uris,
+        public bool $wants_refresh_token,
+        public bool $wants_opaque_token,
+        public bool $wants_pkce,
+        public Signer $signer,
+        public ?string $code_challenge_method,
+        public ?string $code_challenge,
+        public ?string $code_verifier
+    ){}
 }
